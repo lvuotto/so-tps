432b0c76709e4246c264172bdf03b68e658879b4
Federico Hosen <fhosen@hotmail.com>
2014-09-11 21:13:09 -0300

    Finalización código `SchedRR2`.

--- tp1/src/sched_rr2.cpp
+++ tp1/src/sched_rr2.cpp
@@ -2,29 +2,70 @@
 #include <queue>
 #include "sched_rr2.h"
 #include "basesched.h"
-#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 SchedRR2::SchedRR2(vector<int> argn) {
-	// Round robin recibe la cantidad de cores y sus cpu_quantum por parámetro
+// Round robin recibe la cantidad de cores y sus cpu_quantum por parámetro
+  for (int i = 0; i < argn[0]; i++){
+    cores.push_back(core_info(argn[i + 1]));
+  }
 }
 
 SchedRR2::~SchedRR2() {
 
 }
 
+int SchedRR2::min_core() {
+  return min_element(cores.begin(), cores.end(), core_cmp()) - cores.begin();
+}  
 
 void SchedRR2::load(int pid) {
+  pid_a_core.insert(pair<int,int>(pid, min_core()));
+  cores[min_core()].ready.push(pid);
 }
 
 void SchedRR2::unblock(int pid) {
+  cores[pid_a_core[pid]].ready.push(pid);
+  cores[pid_a_core[pid]].bloqueados--;
 }
 
 int SchedRR2::tick(int cpu, const enum Motivo m) {
-  return IDLE_TASK;
-}
-
-int SchedRR2::next(int cpu) {
-	return 0;
+  if (cores[cpu].ready.empty() && !cores[cpu].corriendo){ 
+    return IDLE_TASK;
+  }
+  
+  if (m == EXIT) {
+    pid_a_core.erase(current_pid(cpu));
+    cores[cpu].ciclos = 0;
+    cores[cpu].corriendo = false;
+    if (cores[cpu].ready.empty()) {
+      return IDLE_TASK;
+    }
+  } else if (m == BLOCK) {
+    cores[cpu].bloqueados++;
+    cores[cpu].ciclos = 0;
+    cores[cpu].corriendo = false;
+    if (cores[cpu].ready.empty()) {
+      return IDLE_TASK;
+    }
+  } else {
+    if (current_pid(cpu) == IDLE_TASK) {
+       cores[cpu].ciclos = 0;
+    } else {
+      cores[cpu].ciclos++;
+    }
+    if (cores[cpu].ciclos == cores[cpu].quantum) {
+      cores[cpu].ciclos = 0;
+      cores[cpu].ready.push(current_pid(cpu));
+    } else if (current_pid(cpu) != IDLE_TASK) {
+      return current_pid(cpu);
+    }
+  }
+  
+  cores[cpu].corriendo = true;
+  int n = cores[cpu].ready.front();
+  cores[cpu].ready.pop();
+  return n;
 }
--- tp1/src/sched_rr2.h
+++ tp1/src/sched_rr2.h
@@ -3,18 +3,39 @@
 
 #include <vector>
 #include <queue>
+#include <map>
 #include "basesched.h"
 
-class SchedRR2 : public SchedBase {
-	public:
-		SchedRR2(std::vector<int> argn);
-        ~SchedRR2();
-		virtual void load(int pid);
-		virtual void unblock(int pid);
-		virtual int tick(int cpu, const enum Motivo m);
+using namespace std;
 
-	private:
-		int next(int cpu);
+class SchedRR2 : public SchedBase {
+public:
+  SchedRR2(std::vector<int> argn);
+  ~SchedRR2();
+  virtual void load(int pid);
+  virtual void unblock(int pid);
+  virtual int tick(int cpu, const enum Motivo m);
+
+private:
+  struct core_info {
+    int quantum;
+    int bloqueados;
+    queue<int> ready;
+    int ciclos;
+    bool corriendo;
+    core_info(int q) : quantum(q), bloqueados(0), ciclos(0),
+                       corriendo(false) {}
+  };
+
+  struct core_cmp {
+    bool operator() (const core_info& c1, const core_info& c2) {
+      return (c1.bloqueados + c1.ready.size() + (c1.corriendo ? 1 : 0)) <
+             (c2.bloqueados + c2.ready.size() + (c2.corriendo ? 1 : 0));
+    }
+  };
+  map<int, int> pid_a_core;
+  vector<core_info> cores;
+  int min_core();
 };
 
 #endif
